383f8e4275ba6163aa13fa55a0c0de9a6b734e88
RomainMaillot <romainmaillot.rm@gmail.com>
2019-05-25 19:12:37 +0200

    Display news on scrollable feed

--- app/src/main/java/fr/romainkhanoyan/droidnewsapp/NewsFeedActivity.kt
+++ app/src/main/java/fr/romainkhanoyan/droidnewsapp/NewsFeedActivity.kt
@@ -1,10 +1,18 @@
 package fr.romainkhanoyan.droidnewsapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.mikepenz.fastadapter.FastAdapter
+import com.mikepenz.fastadapter.IItem
+import com.mikepenz.fastadapter.adapters.ItemAdapter
+import fr.romainkhanoyan.droidnewsapp.services.Article
 import fr.romainkhanoyan.droidnewsapp.services.NewsRepository
 import fr.romainkhanoyan.droidnewsapp.services.NewsWrapper
+import fr.romainkhanoyan.droidnewsapp.viewholder.NewsItem
+import kotlinx.android.synthetic.main.activity_news_feed.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -14,6 +22,17 @@ class NewsFeedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news_feed)
+
+        recyclerView.layoutManager = LinearLayoutManager(
+            this,
+            RecyclerView.VERTICAL,
+            false
+        )
+
+        val itemAdapter = ItemAdapter<IItem<*, *>>()
+        val fastAdapter = FastAdapter.with<NewsItem, ItemAdapter<IItem<*, *>>>(itemAdapter)
+
+        recyclerView.adapter = fastAdapter
 
         val repository = NewsRepository()
 
@@ -29,10 +48,12 @@ class NewsFeedActivity : AppCompatActivity() {
                     val newsWrapper = response.body()
 
                     if (newsWrapper != null) {
-                        val status = newsWrapper.status
 
-                        Toast.makeText(this@NewsFeedActivity, "Status = $status", Toast.LENGTH_SHORT).show()
+                        for (_article in newsWrapper.articles) {
+                            val item = NewsItem(_article)
 
+                            itemAdapter.add(item)
+                        }
                     }
 
                 }

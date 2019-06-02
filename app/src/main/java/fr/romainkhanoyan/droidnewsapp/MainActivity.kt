7f48ebf0172551f5785497ff6a92eb4fa85654b8
RomainKH <romain.khanoyan@gmail.com>
2019-06-02 13:30:39 +0200

    :sparkles: country list spinner element done :poop: need to implement list in function of call to api

--- app/src/main/java/fr/romainkhanoyan/droidnewsapp/MainActivity.kt
+++ app/src/main/java/fr/romainkhanoyan/droidnewsapp/MainActivity.kt
@@ -2,6 +2,10 @@ package fr.romainkhanoyan.droidnewsapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,6 +19,18 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, NewsFeedActivity::class.java)
 
             startActivity(intent)
+        }
+
+        val options = arrayListOf("France", "lol","United Kingdom")
+        countrySelector.adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,options)
+        countrySelector.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                Toast.makeText(this@MainActivity, "not selected", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                Toast.makeText(this@MainActivity, options[p2], Toast.LENGTH_LONG).show()
+            }
         }
     }
 }

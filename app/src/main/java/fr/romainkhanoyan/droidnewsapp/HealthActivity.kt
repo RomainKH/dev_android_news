98473f8030fcade28d2c000d4cad64f391ecd739
RomainMaillot <romainmaillot.rm@gmail.com>
2019-06-03 16:33:31 +0200

    Changing category

--- app/src/main/java/fr/romainkhanoyan/droidnewsapp/HealthActivity.kt
+++ app/src/main/java/fr/romainkhanoyan/droidnewsapp/HealthActivity.kt
@@ -1,15 +1,14 @@
 package fr.romainkhanoyan.droidnewsapp
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mikepenz.fastadapter.FastAdapter
 import com.mikepenz.fastadapter.IItem
 import com.mikepenz.fastadapter.adapters.ItemAdapter
-import fr.romainkhanoyan.droidnewsapp.services.Article
 import fr.romainkhanoyan.droidnewsapp.services.NewsRepository
 import fr.romainkhanoyan.droidnewsapp.services.NewsWrapper
 import fr.romainkhanoyan.droidnewsapp.viewholder.NewsItem
@@ -18,11 +17,12 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class NewsFeedActivity : AppCompatActivity() {
+class HealthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news_feed)
+        setContentView(R.layout.activity_health)
+
         var query = "us"
 
         recyclerView.layoutManager = LinearLayoutManager(
@@ -39,59 +39,9 @@ class NewsFeedActivity : AppCompatActivity() {
             query = "fr"
             createContent(query)
         }
-
-        healthButton.setOnClickListener{
-            createContentHealth("us")
-        }
     }
 
     fun createContent(query: String)
-    {
-        val itemAdapter = ItemAdapter<IItem<*, *>>()
-        val repository = NewsRepository()
-        val fastAdapter = FastAdapter.with<NewsItem, ItemAdapter<IItem<*, *>>>(itemAdapter)
-
-        itemAdapter.clear()
-
-        repository.api.getHeadlines(query)
-            .enqueue(object : Callback<NewsWrapper> {
-
-                override fun onFailure(call: Call<NewsWrapper>, t: Throwable) {
-                    t.printStackTrace()
-                    Toast.makeText(this@NewsFeedActivity, "ERROR", Toast.LENGTH_SHORT).show()
-                }
-
-                override fun onResponse(call: Call<NewsWrapper>, response: Response<NewsWrapper>) {
-                    val newsWrapper = response.body()
-
-                    if (newsWrapper != null) {
-
-                        for (_article in newsWrapper.articles) {
-                            val item = NewsItem(_article)
-
-                            itemAdapter.add(item)
-                        }
-                    }
-
-                }
-
-            })
-
-        recyclerView.adapter = fastAdapter
-
-        fastAdapter.withOnClickListener { view, adapter, item, position ->
-            val intent = Intent(this, ArticleActivity::class.java)
-            intent.putExtra("image", item.article.urlToImage)
-            intent.putExtra("title", item.article.title)
-            intent.putExtra("author", item.article.author)
-            intent.putExtra("date", item.article.publishedAt)
-            intent.putExtra("content", item.article.content)
-            startActivityForResult(intent, 1)
-            true
-        }
-    }
-
-    fun createContentHealth(query: String)
     {
         val itemAdapter = ItemAdapter<IItem<*, *>>()
         val repository = NewsRepository()
@@ -104,7 +54,7 @@ class NewsFeedActivity : AppCompatActivity() {
 
                 override fun onFailure(call: Call<NewsWrapper>, t: Throwable) {
                     t.printStackTrace()
-                    Toast.makeText(this@NewsFeedActivity, "ERROR", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@HealthActivity, "ERROR", Toast.LENGTH_SHORT).show()
                 }
 
                 override fun onResponse(call: Call<NewsWrapper>, response: Response<NewsWrapper>) {
